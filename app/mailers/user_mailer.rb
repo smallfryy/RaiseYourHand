8be7a603c8332cd21122b3a89c8cb125df1611c1
chintas1 <chintas1@tcnj.edu>
2016-04-12 12:45:39 -0400

    email bug

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,6 +2,6 @@ class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
     @url = 'http://Raise-Your-Hand.herokuapp.com'
-    mail to @user.email. subject: "Thanks for Logging In"
+    mail(to @user.email, subject: "Thanks for Logging In")
   end
 end
