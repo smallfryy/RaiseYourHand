9664cd09dec592747fc73afabd0ce9595c078be8
chintas1 <chintas1@tcnj.edu>
2016-04-12 12:57:15 -0400

    changed email port

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,7 +21,7 @@ Rails.application.configure do
   #config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :address              => "smtp.gmail.com",
-      :port                 => 25,
+      :port                 => 587,
       :domain               => "gmail.com",
       :user_name            => "raiseyourhandapp@gmail.com",
       :password             => "raiseyourhand555",
